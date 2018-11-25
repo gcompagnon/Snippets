c2fec24f267515aec20c38eb04c2cb7cf904662d
gcompagnon <guilaume_compagnon>
2018-11-26 00:34:48 +0100

    sample of DFS

--- SnippetsApp/Program.cs
+++ SnippetsApp/Program.cs
@@ -11,7 +11,32 @@ namespace SnippetsApp
         static void Main(string[] args)
         {
             Console.Out.WriteLine("Hello World!");
+            
+            String s = "Hello   the       World some of the  trick and   tips!";
+            Console.Out.WriteLine(s.TrimAndReduce());
+
+            //BFSUtil
+
             Console.ReadKey();
         }
     }
+
+
+    //Extension methods must be defined in a static class
+    public static class StringExtension
+    {
+        // This is the extension method.
+        // The first parameter takes the "this" modifier
+        // and specifies the type for which the method is defined.
+        public static string TrimAndReduce(this string str)
+        {
+            return ConvertWhitespacesToSingleSpaces(str).Trim();
+        }
+
+        public static string ConvertWhitespacesToSingleSpaces(this string value)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(value, @"\s+", " ");
+        }
+    }
+
 }
